c6f621e23ae0d63f4123ee93bc0da33bf13b167d
Hendi Santika <hendisantika@gmail.com>
2022-04-03 14:39:13 +0700

    #11 - Add Student Model

---
+++ src/main/java/com/hendisantika/model/Student.java
@@ -0,0 +1,20 @@
+package com.hendisantika.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springdoc-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/04/22
+ * Time: 14.38
+ */
+@Getter
+@Setter
+public class Student {
+    private String name;
+    private Long id;
+}
