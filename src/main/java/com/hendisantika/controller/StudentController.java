49addbfd9d2c7f7e001b1c07f4ef50ed6078d006
Hendi Santika <hendisantika@gmail.com>
2022-04-03 14:41:08 +0700

    #13 - Add getStudents API in StudentController

--- src/main/java/com/hendisantika/controller/StudentController.java
+++ src/main/java/com/hendisantika/controller/StudentController.java
@@ -1,7 +1,17 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.Student;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +26,27 @@ import org.springframework.web.bind.annotation.RestController;
         name = "Student Resource")
 @RestController("/")
 public class StudentController {
+    @Operation(summary = "Get students",
+            description = "Provides all available students list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "${api.response-codes.ok.desc}"),
+            @ApiResponse(responseCode = "400",
+                    description = "${api.response-codes.badRequest.desc}",
+                    content = {@Content(examples = {@ExampleObject(value = "")})}),
+            @ApiResponse(responseCode = "404",
+                    description = "${api.response-codes.notFound.desc}",
+                    content = {@Content(examples = {@ExampleObject(value = "")})})})
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        List<Student> students = new ArrayList<>();
+
+        Student student = new Student();
+        student.setId(1L);
+        student.setName("Arun");
+        students.add(student);
+
+        return students;
+    }
 }
