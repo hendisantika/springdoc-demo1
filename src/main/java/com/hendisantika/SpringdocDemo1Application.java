18eda82b06521a25fb6fac422246d7cd69c5c232
Hendi Santika <hendisantika@gmail.com>
2022-04-03 14:07:55 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringdocDemo1Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocDemo1Application.class, args);
+    }
+
+}
